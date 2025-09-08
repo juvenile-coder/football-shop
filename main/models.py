21d506d433394d18a5c94e7cc36d66fe0f5c37cc
juvenile-coder <jefftirza@gmail.com>
2025-09-08 23:34:51 +0700

    Revisi pada models.py

--- main/models.py
+++ main/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('tshirt', 'T-Shirt'),
+        ('jacket', 'Jacket'),
+        ('shorts', 'Shorts')
+        ('shoes', 'Shoes'),
+        ('accessories', 'Accessories'),
+    ]
+
     name = models.CharField()
     price = models.IntegerField()
     description = models.TextField()
-    thumbnail = models.URLField()
-    category = models.CharField()
-    is_featured = models.BooleanField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='update')
+    is_featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
